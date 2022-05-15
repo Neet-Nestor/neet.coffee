3f38b9395c1e517ae18e4fb8ee3a8ec948e579d5
Nestor Qin <imba.qxy@gmail.com>
2022-05-15 01:26:49 -0400

    Fix the aspect ratio issue on Safari

--- pages/Bio.tsx
+++ pages/Bio.tsx
@@ -7,7 +7,7 @@ interface BioAvatarProps {
 
 const BioAvatar: React.FC<BioAvatarProps> = ({ imageName }) => {
   return (
-    <div className="relative my-6 mx-auto w-[80%] max-w-xs aspect-square border-[18px] border-white rounded-full overflow-clip shadow-[0_0_25px_0_rgba(0,0,0,0.1)] md:max-w-xs md:flex-none md:mx-0">
+    <div className="relative my-6 mx-auto min-w-min w-[80%] max-w-xs flex aspect-square border-[18px] border-white rounded-full overflow-clip shadow-[0_0_25px_0_rgba(0,0,0,0.1)] md:max-w-xs md:flex-none md:mx-0">
       <Image
         src={`/images/${imageName}`}
         alt="Avatar"
@@ -21,9 +21,10 @@ const BioAvatar: React.FC<BioAvatarProps> = ({ imageName }) => {
 
 const Bio: React.FC = () => {
   return (
-    <div className="pt-11 min-h-screen relative flex justify-center items-center bg-white mx-10 lg:min-h-max">
-      <div className="py-10 flex flex-col justify-center md:flex-row grow items-center md:gap-10">
+    <div className="pt-11 min-h-screen min-w-[240px] relative flex justify-center items-center bg-white mx-10 lg:min-h-max">
+      <div className="py-10 flex flex-col justify-center -mt-10 grow md:flex-row items-center md:gap-10">
         <BioAvatar imageName="avatar.jpeg" />
+        {/* <BioAvatar imageName="pins-photo.JPG" /> */}
         <div className="text-left inline-block leading-loose md:text-left md:flex md:flex-col md:justify-center">
           <p className="text-3xl">
             Hi, I&apos;m <strong>Nestor Qin</strong>
