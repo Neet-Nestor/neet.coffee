9151fcc9aa50015982a359431f1178d6b16102e8
Nestor Qin <imba.qxy@gmail.com>
2024-05-12 22:54:39 -0400

    Add PWA and favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,19 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Nestor Qin, Full-Stack Engineer</title>
+        <meta
+          name="description"
+          content="Nestor Qin is a versatile full-stack engineer who turns coffee into innovative Web, Mobile, and AI Applications."
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        ></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -20,6 +30,27 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
         ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d9a577" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#f2e2ce" />
       </Head>
       <body>
         <noscript>
@@ -32,6 +63,19 @@ export default function Document() {
         </noscript>
         <Main />
         <NextScript />
+        <Script
+          id="Google Tag Manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-KJJ2D43');
+    `,
+          }}
+        />
       </body>
     </Html>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,25 @@
 import type { NextPage } from "next"
-import Head from "next/head"
-import Script from "next/script"
 import Bio from "../components/Bio"
 import TechStack from "../components/TechStack"
 import NavigationBar from "../components/NavigationBar"
-import UnderConstruction from "../components/UnderConstruction"
 import styles from "../styles/index.module.css"
 import Projects from "../components/Projects"
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Nestor Qin, Full-Stack Engineer-</title>
-        <meta
-          name="description"
-          content="Nestor Qin's personal portfolio website"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        ></meta>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="relative min-w-[21rem] lg:min-h-screen lg:p-10">
-        <div className={`${styles.background}`}></div>
-        <div
-          id="page-container"
-          className="relative min-w-full overflow-hidden bg-white lg:min-w-min lg:min-h-[80%] lg:max-w-[94%] lg:mx-auto lg:rounded-3xl xl:max-w-[80rem]"
-        >
-          <NavigationBar />
-          <Bio />
-          <TechStack />
-          <Projects />
-          {/* <UnderConstruction /> */}
-        </div>
-      </main>
-
-      <Script
-        id="Google Tag Manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KJJ2D43');
-          `,
-        }}
-      />
-    </>
+    <main className="relative min-w-[21rem] lg:min-h-screen lg:p-10">
+      <div className={`${styles.background}`}></div>
+      <div
+        id="page-container"
+        className="relative min-w-full overflow-hidden bg-white lg:min-w-min lg:min-h-[80%] lg:max-w-[94%] lg:mx-auto lg:rounded-3xl xl:max-w-[80rem]"
+      >
+        <NavigationBar />
+        <Bio />
+        <TechStack />
+        <Projects />
+        {/* <UnderConstruction /> */}
+      </div>
+    </main>
   )
 }
 
